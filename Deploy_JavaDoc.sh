d59ac54bf8b12b0e94ee3950c1673acb884dea57
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-04-03 10:12:43 -0300

    Updates deploy script to latest.

--- Deploy_JavaDoc.sh
+++ Deploy_JavaDoc.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
+PROJECT_NAME="VDice"
+
 # Display Repo:
 echo -e "Repo Slug:"
-echo -e "\tExpected:\tValkryst/VDice"
-echo -e "\tActual:\t$TRAVIS_REPO_SLUG\n"
+echo -e "\tExpected: Valkryst/${PROJECT_NAME}"
+echo -e "\tActual: $TRAVIS_REPO_SLUG\n"
 
 # Display JDK Version:
 echo -e "JDK Version:"
-echo -e "\tExpected:\toraclejdk8"
-echo -e "\tActual:\t$TRAVIS_JDK_VERSION\n"
+echo -e "\tExpected: oraclejdk8"
+echo -e "\tActual: $TRAVIS_JDK_VERSION\n"
 
 # Display 'Is Pull Request':
 echo -e "Is Pull Request:"
-echo -e "\tExpected:\tfalse"
-echo -e "\tActual:\t$TRAVIS_PULL_REQUEST\n"
+echo -e "\tExpected: false"
+echo -e "\tActual: $TRAVIS_PULL_REQUEST\n"
 
 # Display Branch:
 echo -e "Branch:"
-echo -e "\tExpected:\tfalse"
-echo -e "\tActual:\t$TRAVIS_BRANCH\n"
+echo -e "\tExpected: master"
+echo -e "\tActual: $TRAVIS_BRANCH\n"
 
-if [ "$TRAVIS_REPO_SLUG" == "Valkryst/VDice" ] &&
+if [ "$TRAVIS_REPO_SLUG" == "Valkryst/"${PROJECT_NAME} ] &&
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
    [ "$TRAVIS_BRANCH" == "master" ]; then
-   
+
     echo -e "Publishing JavaDoc...\n"
 
     cd $HOME
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Valkryst/VDice gh-pages > /dev/null
-    
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Valkryst/${PROJECT_NAME} gh-pages > /dev/null
+
     cd gh-pages
     git rm -r *
     git config user.name "Valkryst"
     git config user.email "valkryst@valkryst.com"
-    cp -r /home/travis/build/Valkryst/VDice/target/site/apidocs/* .
+    cp -r /home/travis/build/Valkryst/${PROJECT_NAME}/target/site/apidocs/* .
     git add .
     git commit -m "Updates JavaDoc on successful Travis CI build. Build #$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages."
     git push -fq origin gh-pages > /dev/null
